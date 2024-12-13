44ec80518830f5bcad4a5e471063eda5fe42c12c
cauaschuch <caua.ferrazza@gmail.com>
2024-12-12 21:21:22 -0300

    roots

--- roots.f90
+++ roots.f90
@@ -1,1 +1,24 @@
-!testing
+program newton
+implicit none
+real,external::f,df
+integer::i
+real::a,thr
+thr = 1d-10
+i = 0
+a = -1
+do while (abs(f(a)) > thr .and. i < 100)
+a = a - f(a)/df(a)
+i = i+1
+end do
+print*, (a),i
+
+
+end
+real function f(x)
+real::x
+f = x**2 +4 - x**3-x**4
+end function f 
+real function df(x)
+real::x
+df = 2*x -3*x**2 -4*x**3
+end function df
